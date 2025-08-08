2e52a0d659d544b061d5d6db3ef2b86dfc532153
gunthersandigan@gmail.com <gunthersandigan@gmail.com>
2025-08-08 15:13:27 +0800

    I modified banking and account

--- src/Banking/Bankapp.java
+++ src/Banking/Bankapp.java
@@ -3,26 +3,31 @@ package Banking;
 import java.util.Scanner;
 
 public class Bankapp {
-    int accountNo = 1008;
-    String name;
-    float initialBalance = 0;
-    int pin = 2006;
+    int accountNo;
+    float initialBalance;
+    int pin;
     
-    public boolean verifyAccount(int acc, int pn){
-        
-            if(acc == accountNo && pn == pin){
-                return true;
-            }else{
-                return false;
-            }
+    public Bankapp() {
+        this.initialBalance = 0.0f; 
     }
     
-    public int setAccount(){
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public int getAccountNo() {
         return accountNo;
     }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public int getPin() {
+        return pin;
+    }
     
-    public void viewBalance(){
-    
-    
+    public boolean verifyAccount(int acc, int pn){
+        return acc == this.accountNo && pn == this.pin;
     }
 }
